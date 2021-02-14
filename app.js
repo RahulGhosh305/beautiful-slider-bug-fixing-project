8cf754142c3e18c7286db455d58f953dc9dfdc39
Rahul Ghosh <rahulghosh096@gmail.com>
2021-02-14 15:51:30 +0600

    v1.5 silder picture select and deselect fixed

--- app.js
+++ app.js
@@ -38,13 +38,16 @@ const getImages = (query) => {
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
+  element.classList.toggle('added');
  
   let item = sliders.indexOf(img);
+  // console.log(item)
   if (item === -1) {
     sliders.push(img);
+    // console.log(sliders)
   } else {
-    alert('Hey, Already added !')
+    // alert('Hey, Already added !')
+    sliders.pop(img)
   }
 }
 var timer
